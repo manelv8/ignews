3f0d2d5fcde1664d6df76339a4ff309a7c7180ea
davi <manelv8@gmail.com>
2021-04-16 06:29:25 -0300

    add user to faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import  NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 export default NextAuth({
   providers:[
     Providers.GitHub({
@@ -7,5 +11,41 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       scope:'read:user'
     })
-  ]
+  ],
+  callbacks:{
+    async signIn(user,account,profile){
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        );
+
+      return true;
+
+      } catch {
+        return false
+        
+      }
+    }
+  }
 })
